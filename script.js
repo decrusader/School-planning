499a11ca10409c8dbe9d11536641c4d5a269b7b8
Decrusader <samdewaele10@gmail.com>
2024-11-05 14:08:15 +0100

    verandering(update)

--- script.js
+++ script.js
@@ -22,24 +22,18 @@ setInterval(function () {
 }, 100);
 
 const schedule = [
-    { time: '7:45', activity: 'nietvroeger douchen' },
-    { time: '8:15', activity: 'wekken' },
-    { time: '8:30', activity: 'eten' },
-    { time: '9:15', activity: 'verzamelen arena - lesmoment 1' },
-    { time: '10:45', activity: 'pauze bar' },
-    { time: '11:05', activity: 'lesmoment 2' },
-    { time: '12:30', activity: 'middageten en vrij' },
-    { time: '13:45', activity: 'verzamelen arena - lesmoment 3' },
-    { time: '15:15', activity: 'pauze - verzamelen' },
-    { time: '15:20', activity: 'verzamelen bronne - start namiiddagactiviteiten' },
-    { time: '17:45', activity: 'vrij' },
-    { time: '18:30', activity: 'avondeten - vrij' },
-    { time: '19:45', activity: 'verzamelen arena - start avondactiviteiten' },
-    { time: '21:30', activity: 'vrij - bar' },
-    { time: '22:00', activity: 'niet meer douchen' },
-    { time: '22:00', activity: 'slapen gaan 1e graad blauw bandje' },
-    { time: '22:30', activity: 'slapen gaan 2e graad groen bandje' },
-    { time: '22:45', activity: 'slapen gaan 3e graad rood bandje' },
+    { time: '8:30', activity: '' },
+    { time: '9:20', activity: '' },
+    { time: '10:10', activity: 'Speeltijd' },
+    { time: '10:25', activity: '' },
+    { time: '11:15', activity: '' },
+    { time: '12:05', activity: 'Middagpauze' },
+    { time: '13:10', activity: '' },
+    { time: '14:00', activity: '' },
+    { time: '14:50', activity: 'Speeltijd' },
+    { time: '15:05', activity: '' },
+    { time: '15:55', activity: '' },
+    { time: '16:45', activity: 'vrij' },
 ]
 
 let betterschedule = [];
